630d91b3b8d9b4297f2cac96b5b336bb01ec70cd
wellingtonmacena <wellington.macena.23@gmail.com>
2023-09-06 00:26:40 -0300

    Removed unused code

--- Moments-Backend/Resources/Database Scripts/comments.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE COMMENTS (
-  id SERIAL PRIMARY KEY,
-  username TEXT NOT NULL,
-  text TEXT NOT NULL,
-  moment_id INT NOT NULL,
-  created_at timestamp with time zone DEFAULT now() NOT NULL,
-  updated_at timestamp with time zone
-)
--- Moments-Backend/Resources/Database Scripts/moments.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE moments (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  description TEXT NOT NULL,
-  image_url TEXT NOT NULL,
-  image_path text NOT NULL,
-  created_at timestamp with time zone DEFAULT now() NOT NULL,
-  updated_at timestamp with time zone
-)
